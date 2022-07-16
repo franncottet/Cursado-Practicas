a8caab45495dbf990beae5e87b4ff036f054d2e9
franncottet <cottetfranco@gmail.com>
2022-07-16 15:21:36 -0300

    cambios

--- bluuweb.js
+++ bluuweb.js
@@ -1,40 +1,30 @@
-// CALLBACKS, PROMESAS Y ASYNC AWAIT
+const carrito = document.getElementById("carrito")
+const template = document.getElementById("template")
+const fragment= document.createDocumentFragment()
+const botones= document.querySelectorAll(".card .btn")
 
-const posts = [
-    {
-        userId: 1,
-        id: 1,
-        title:
-            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body:
-            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    },
-    {
-        userId: 1,
-        id: 2,
-        title: "qui est esse",
-        body:
-            "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-    {
-        userId: 1,
-        id: 3,
-        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        body:
-            "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-    },
-];
+console.log(carrito);
+console.log(template);
+console.log(fragment);
+console.log(botones);
 
-const findPostById = (id, callback) =>{
-    const post = posts.find(item => item.id === id)
+const carritoObjeto = {}
 
-    callback(post)
+const agregarProducto = (e) => {
+console.log(e.target.dataset.fruta);
+
+const producto= {
+    titulo: e.target.dataset.fruta,
+    id: e.target.dataset.fruta,
+    cantidad: 1
+}
+
+carritoObjeto[producto.titulo]=producto
 
 }
 
-findPostById(1, (post)=>{
-    console.log(post);
-})
 
-//AJAX, FETCH
+
+
+botones.forEach((btn) => btn.addEventListener("click", agregarProducto))
 
